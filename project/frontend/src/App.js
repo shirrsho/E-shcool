68800df6d9f023930ffcb63c72aca3b19ddfce8d
Hasnain Iqbal <60136067+shirsho1106@users.noreply.github.com>
2022-04-18 18:09:38 +0600

    mid2 1

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import AdminDash from "./components/AdminDash";
+import RegisterTeacher from "./components/RegisterTeacher";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/studentdashboard">
+            <Navbar />
+            <Dashboard />
+          </Route>
+          <Route exact path="/admindashboard">
+            <AdminDash />
+          </Route>
+          <Route exact path="/registerteacher">
+            <RegisterTeacher />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ project/frontend/src/components/Noticeboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Notices from "./Notice";
+
+const Noticeboard = () => {
+  return (
+    <div>
+      <Notices />
+    </div>
+  );
+};
+
+export default Noticeboard;
